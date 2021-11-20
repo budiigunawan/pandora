5fadcf734e8212460419308f6f34cce2985ebdac
Budi I. Gunawan <budiindra.g@gmail.com>
2021-11-20 09:16:15 +0700

    initial commit for pandora

---
+++ src/components/HomepageFeatures.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './HomepageFeatures.module.css';
+
+const FeatureList = [
+  {
+    title: 'Mudah Dipelajari',
+    Svg: require('../../static/img/undraw_programmer_re_owql.svg').default,
+    description: (
+      <>
+        Docusaurus was designed from the ground up to be easily installed and
+        used to get your website up and running quickly.
+      </>
+    ),
+  },
+  {
+    title: 'Percepat Development',
+    Svg: require('../../static/img/undraw_maker_launch_crhe.svg').default,
+    description: (
+      <>
+        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
+        ahead and move your docs into the <code>docs</code> directory.
+      </>
+    ),
+  },
+  {
+    title: 'Best Practices',
+    Svg: require('../../static/img/undraw_book_lover_re_rwjy.svg').default,
+    description: (
+      <>
+        Extend or customize your website layout by reusing React. Docusaurus can
+        be extended while reusing the same header and footer.
+      </>
+    ),
+  },
+];
+
+function Feature({ Svg, title, description }) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className='text--center'>
+        <Svg className={styles.featureSvg} alt={title} />
+      </div>
+      <div className='text--center padding-horiz--md'>
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className='container'>
+        <div className='row'>
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}

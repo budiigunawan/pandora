ed34dcc154082195626155b9d9ef2e0af78fa627
Budi I. Gunawan <budiindra.g@gmail.com>
2022-01-30 23:30:42 +0700

    change docs/background

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -70,7 +70,7 @@ const config = {
             items: [
               {
                 label: 'Documentation',
-                to: '/docs/intro',
+                to: '/docs/background',
               },
             ],
           },
